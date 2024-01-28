1676c6e90e2ba4620f3d83961dfd39dfbabfe95b
Senziousjs <4563461+Senziousjs@users.noreply.github.com>
2024-01-28 17:30:34 +0100

    Update themeswitch.js

--- js/module/themeswitch.js
+++ js/module/themeswitch.js
@@ -1,5 +1,6 @@
 (function($) {
   $.fn.themeSwitch = function(theme) {
+    // Add new Themes here (and create a css file in the css/theme directory with the exact same name in the html data-theme attribute)
     const Themes = [
       {
         name: 'Classic',
@@ -8,7 +9,7 @@
       {
         name: 'Classic Blue',
         color: '#b2b4e3'
-      }
+      },
     ]
     const ThemeHandler = {
       init: () => $('html').attr('data-theme', ThemeHandler.getTheme()),
@@ -21,14 +22,12 @@
         $('html').attr('data-theme', theme)
       }
     }
-
-    ThemeHandler.init()
-
     Themes.map(({ name, color }) => $(this).append(`
       <div name="${name}" 
         style="display:inline-block;width:20px;height:20px;box-shadow:0 0 10px 0 rgba(0, 0, 0, .1);background:${color};border-radius:var(--border-radius);border:1px solid white;"
       />
     `))
     $(this).find('div').each((_, el) => $(el).bind('click', (e) => ThemeHandler.setTheme($(e.currentTarget).attr('name'))))
+    ThemeHandler.init()
   }
 }(jQuery))
